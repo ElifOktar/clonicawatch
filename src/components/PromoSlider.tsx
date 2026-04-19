99e0ac045266cff9e8f45164e193c063858c221b
ElifOktar <159653793+ElifOktar@users.noreply.github.com>
2026-04-19 19:27:48 +0300

    Update PromoSlider.tsx

--- src/components/PromoSlider.tsx
+++ src/components/PromoSlider.tsx
@@ -236,7 +236,7 @@ export function PromoSlider() {
 
       {/* Dots */}
       {slides.length > 1 && (
-        <div className="absolute bottom-3 left-1/2 -translate-x-1/2 z-20 flex gap-2">
+        <div className="absolute bottom-3 left-1/2 -translate-x-1/2 z-20 hidden md:flex gap-2">
           {slides.map((_, i) => (
             <button
               key={i}
